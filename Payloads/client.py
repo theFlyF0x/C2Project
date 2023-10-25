39fb200c61a56bebdba5737344f5b292020eb149
theFlyF0x <fedesesana@gmail.com>
2023-10-25 17:13:41 +0200

    Connection check

--- Payloads/client.py
+++ Payloads/client.py
@@ -1,1 +1,8 @@
 import socket
+
+HOST = "..."
+PORT = "6969"
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    sock.connect((HOST, PORT))
+    sock.sendall(b"ciao")
--- Server/main.py
+++ Server/main.py
@@ -1,2 +1,13 @@
 import socket
 
+HOST = "127.0.0.1"
+PORT = "6969"
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    sock.bind((HOST, PORT))
+    sock.listen(1)
+    conn, addr = sock.accept()
+    with conn: 
+        print(f"Connesso a {addr}")
+        data = conn.recv(1024)
+        print(data)
