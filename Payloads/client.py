8128aec5366ff0bac0f84bd733f02cb9737f7e46
FlyFox717 <fedesesana@gmail.com>
2023-10-26 20:09:57 +0200

    Began the shell function for the server

--- Payloads/client.py
+++ Payloads/client.py
@@ -1,8 +1,8 @@
 import socket
 
-HOST = "..."
-PORT = "6969"
+HOST = "127.0.0.1" # C2 Server IP
+PORT = 6969 # C2 application port
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.connect((HOST, PORT))
+    sock.connect((HOST, PORT)) # Connect to the C2 server
     sock.sendall(b"ciao")
--- Server/main.py
+++ Server/main.py
@@ -1,13 +1,25 @@
 import socket
 
-HOST = "127.0.0.1"
-PORT = "6969"
+HOST = "127.0.0.1" # Address to listen on
+PORT = 6969 # Port to listen on. (Better to use unprivileged ports)
+
+
+def shell(): 
+    """Actions performed when the shell command is typed"""
+    print("[INFO] Dropping into a shell...")
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     sock.bind((HOST, PORT))
-    sock.listen(1)
-    conn, addr = sock.accept()
+    print(f"[INFO] Listening on {HOST}:{PORT}")
+    sock.listen(1) # Wait for an incoming connection
+    conn, addr = sock.accept() # Accept the connection. Store the connection and the IP address of the client connecting
     with conn: 
-        print(f"Connesso a {addr}")
-        data = conn.recv(1024)
-        print(data)
+        print(f"[INFO] Connection received from {addr}\n")
+        while True:
+            command = input("SHELL> ") # Request user command input
+            match command: # Parse user command input
+                case 'shell':
+                    shell()
+            data = conn.recv(1024)
+            print(data)
